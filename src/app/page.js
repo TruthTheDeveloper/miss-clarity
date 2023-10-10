344ea483178fccd9fc05cf0d33d4a891742e00c0
Henry Joshua <henrysempire111@gmail.com>
2023-10-10 22:55:40 +0100

    added favicon and made some corrections

--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,39 @@
-import Navbar from '@/organisms/LandingPage/NavBar'
-import Header from '@/organisms/LandingPage/Header'
-import Prize from '@/organisms/LandingPage/Prize'
-import Contest from '@/organisms/LandingPage/Contest'
-import Participate from '@/organisms/LandingPage/Participate'
-import Rules from '@/organisms/LandingPage/Rules'
-import ReceivePrize from '@/organisms/LandingPage/ReceivePrize'
-import ContactUs from '@/organisms/LandingPage/ContactUs'
-import Frequently from '@/organisms/LandingPage/Frequently'
-import Footer from '@/organisms/LandingPage/Footer'
-import Sponsors from '@/organisms/LandingPage/Sponsors'
-import PreviousWinner from '@/organisms/LandingPage/PreviousWinner'
-import CurrentWinner from '@/organisms/LandingPage/CurrentWinner'
-
+import Navbar from "@/organisms/LandingPage/NavBar";
+import Header from "@/organisms/LandingPage/Header";
+import Prize from "@/organisms/LandingPage/Prize";
+import Contest from "@/organisms/LandingPage/Contest";
+import Participate from "@/organisms/LandingPage/Participate";
+import Rules from "@/organisms/LandingPage/Rules";
+import ReceivePrize from "@/organisms/LandingPage/ReceivePrize";
+import ContactUs from "@/organisms/LandingPage/ContactUs";
+import Frequently from "@/organisms/LandingPage/Frequently";
+import Footer from "@/organisms/LandingPage/Footer";
+import Sponsors from "@/organisms/LandingPage/Sponsors";
+import PreviousWinner from "@/organisms/LandingPage/PreviousWinner";
+import CurrentWinner from "@/organisms/LandingPage/CurrentWinner";
 
 export default function Home() {
   return (
+    <>
+    
     <main className="min-h-screen bg-[white] text-text-black  overflow-x-hidden">
-      <Navbar/>
-      <Header/>
-      <Sponsors/>
-      <PreviousWinner/>
-      <CurrentWinner/>
-      <Prize/>
-      <Contest/>
-      <Participate/>
-      <Rules/>
-      <div className='md:flex md:justify-between'>
-      <ReceivePrize/>
-      <ContactUs/>
+      
+      <Navbar />
+      <Header />
+      <Sponsors />
+      <PreviousWinner />
+      <CurrentWinner />
+      <Prize />
+      <Contest />
+      <Participate />
+      <Rules />
+      <div className="md:flex md:justify-between">
+        <ReceivePrize />
+        <ContactUs />
       </div>
-      <Frequently/>
-      <Footer/>
+      <Frequently />
+      <Footer />
     </main>
-  )
+    </>
+  );
 }
