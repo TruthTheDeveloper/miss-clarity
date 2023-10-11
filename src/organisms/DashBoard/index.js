b46f15da4b046cbf481682a9c3372e7b6d0b460c
Henry Joshua <henrysempire111@gmail.com>
2023-10-11 23:29:54 +0100

    fixed the dash issue

--- src/organisms/DashBoard/index.js
+++ src/organisms/DashBoard/index.js
@@ -58,7 +58,7 @@ const DashBoard = () => {
 
   useEffect(() => {
     if (data) {
-      setValue(`https://miss-elegance.vercel.app/${data?.firstName.replace(/["']/g, '')} - ${data?.lastName.replace(/["']/g, '')}/${id.replace(/["']/g, '')}`);
+      setValue(`https://miss-elegance.vercel.app/${data?.firstName.replace(/["']/g, '')}-${data?.lastName.replace(/["']/g, '')}/${id.replace(/["']/g, '')}`);
     }
   }, [data]);
 
